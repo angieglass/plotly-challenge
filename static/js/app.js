f49e5ebfde607430a71aefdd618d6faa392a1dd4
angiecruz <angiecruzalmanza@gmail.com>
2021-07-03 22:16:25 -0500

    gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -33,7 +33,7 @@ function getData() {
     // console.log(userId);
     // console.log(sample_values);
     // console.log(otu_ids);
-    console.log(otu_labels);
+    // console.log(otu_labels);
     // console.log(data);
 
 // Create the Trace for the Bar Chart 
@@ -49,11 +49,13 @@ function getData() {
 // Define the plot layout
 var layoutBar = {
   title: "Top 10 Bacteria Cultures Found",
+  titlefont: {size: 24},
 };
 
-// Plot the chart to a div tag with id "bar-plot"
+// Plot the chart to a div tag with id "bar"
 Plotly.newPlot("bar", dataBar, layoutBar);
 
+// ------------------------------------------------- 
 // Create the Trace for the Bubble Chart 
 var trace1 = {
   x: data.samples[0].otu_ids,
@@ -68,20 +70,67 @@ var trace1 = {
 };
 var dataBubble = [trace1];
 
-// Define the plot layout
+// Define the Bubble layout
 var layoutBubble = {
 title: "Bacteria Cultures Per Samble",
+titlefont: {size: 24},
+xaxis: {title: "OTU ID"}
 
 };
 
-// Plot the chart to a div tag with id "bar-plot"
+// Plot the chart to a div tag with id "bubble"
 Plotly.newPlot("bubble", dataBubble, layoutBubble);
 
+// Create the Trace for the Gauge Chart 
+
+var dataGauge = [{
+  domain: { x: [0, 1], y: [0, 1] },
+  value: data.metadata[0].wfreq,
+  title: { text: "Scrubs per week" },
+  type: "indicator",
+  mode: "gauge",
+  gauge: {
+    axis: { range: [null, 9] },
+  threshold: {
+      line: { color: "#8B008B", width: 5 },
+      thickness: 0.75,
+      value: data.metadata[0].wfreq},
+  steps: [
+    { range: [0, 9], color: "#DCDCDC" },
+    { range: [1, 2], color: "#DCDCDC" },
+    { range: [2, 3], color: "#D3D3D3" },
+    { range: [3, 4], color: "#D3D3D3" },
+    { range: [4, 5], color: "#C0C0C0" },
+    { range: [5, 6], color: "#C0C0C0" },
+    { range: [6, 7], color: "#A9A9A9" },
+    { range: [7, 8], color: "#A9A9A9" },
+    { range: [8, 9], color: "#808080" }],
+
+  }
+}];
+
+// Define the Bubble layout
+var layoutBubble = {
+title: "Belly Button Washing Frequency",
+titlefont: {size: 24},
+};
+
+// Plot the chart to a div tag with id "bubble"
+Plotly.newPlot("gauge", dataGauge, layoutBubble);
 
   })
 }
 
 
+// Obtain the medadata
+function demographics (){
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata[0]; 
+    console.log(metadata); 
+  })
+}; 
+
 // Run function 
+demographics();
 getData();
 optionChanged(); 
